a4cb5ee4afc8a34994ead2dfc598b6cf23d1be17
MustafaTaslialann <100672502+MustafaTaslialann@users.noreply.github.com>
2025-07-21 02:17:28 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const companyInfo = {
+  name: "Mustafa Taşlıalan",
+  brand: "Beyaz Eşya Servisi",
+  location: "Mersin / Silifke",
+  phone: "05065019612",
+  email: "mustafataslialan@outlook.com",
+};
+
+export default companyInfo;
