c065d42fd6c812b026b0212e888e9fc4761d20c0
MustafaTaslialann <100672502+MustafaTaslialann@users.noreply.github.com>
2025-07-21 18:08:23 +0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const companyInfo = {
-  name: "Mustafa Taşlıalan",
-  brand: "Beyaz Eşya Servisi",
-  location: "Mersin / Silifke",
-  phone: "05065019612",
-  email: "mustafataslialan@outlook.com",
-};
-
-export default companyInfo;
